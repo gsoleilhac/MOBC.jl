65722bdbf63224b89684f2482c980f29accdd0c3
Gauthier Soleilhac <gsoleilhac@gmail.com>
2018-02-03 20:29:51 +0100

    WIP

--- src/MOBC.jl
+++ src/MOBC.jl
@@ -1,3 +1,79 @@
 module MOBC
-using vOptGeneric
+using JuMP, vOptGeneric
+
+@enum NODE_STATUS SOLVED BOUNDED PENDING
+
+mutable struct Node
+	m::JuMP.Model
+	status::NODE_STATUS
+	z::Float64
+	x::Vector{Float64}
+	f0::Vector{Int}
+	f1::Vector{Int}
+	feasible::Bool
+	seen::Vector{Vector{Int}}
+	left::Node
+	right::Node
+	Node(m,z,x,f0,f1,seen)=new(m,SOLVED,z,x,f0,f1,true,seen)
+	Node(m,f0,f1,seen)=new(m,PENDING,0,[],f0,f1,true,seen)
+end
+
+Base.show(io::IO, n::Node) = begin
+	print(io, "Node(\t$(n.status), \n")
+	n.status != PENDING && println(io, "\tz=$(n.z)")
+	n.status != PENDING && println(io, "\tx=$(n.x)")
+	println(io, "\tf0=$(n.f0)")
+	println(io, "\tf1=$(n.f1)")
+	n.status != PENDING && println(io, "\tfeasible:$(n.feasible)")
+	isdefined(n, :left) && println(io, "\thas left child")
+	isdefined(n, :right) && println(io, "\thas right child")
+	print(io, ")")
+end
+
+isonlybinary(m, obj) = all(m.colCat[map(x->getfield(x, :col), obj.aff.vars)] .== :Bin)
+hasinteger(m, obj) = any(m.colCat[map(x->getfield(x, :col), obj.aff.vars)] .== :Int)
+
+function solve_BC(vm)
+	vd = getvOptData(vm)
+	@assert length(vd.objs) == 2
+	@assert isonlybinary(vm, vd.objs[1])
+	@assert !hasinteger(vm, vd.objs[1]) && !hasinteger(vm, vd.objs[2])
+
+	if vd.objSenses[1] == :Max
+		vd.objSenses[1] = :Min
+		vd.objs[1] = -1 * vd.objs[1]
+	end
+	if vd.objSenses[2] == :Max
+		vd.objSenses[2] = :Min
+		vd.objs[2] = -1 * vd.objs[2]
+	end
+
+
+	z1, z2 = vd.objs
+	X = find(x->x==:Bin, vm.colCat)
+	Y = find(x->x==:Cont, vm.colCat)
+
+	vm_lex = copy(vm)
+	status = solve(vm_lex, method=:lex)
+	status == :Optimal || return status
+	zUL, zLR = getY_N(vm_lex)
+	@show zUL, zLR
+
+	z = (z1 / (zLR[1]-zUL[1])) + (z2 / (zUL[2]-zLR[2])) - (zUL[1] / (zLR[1] - zUL[1])) - (zLR[2] / (zUL[2] - zLR[2]))
+	
+	JuMP.setobjective(vm, :Min, z.aff)
+
+	res = solve(vm, ignore_solve_hook=true, relaxation=true)
+	res = solve(vm, ignore_solve_hook=true, relaxation=true)
+
+	zlp = getobjectivevalue(vm)
+	@show zlp
+	xlp = vm.colVal
+	@show xlp
+
+	root = Node(vm, zlp, xlp, [], [], [])
+
+	return vm, root
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using MOBC, vOptGeneric, GLPKMathProgInterface
 using Base.Test
 
+m = vModel(solver = GLPKSolverMIP())
 
-# write your own tests here
-@test 1 == 1
+p1 = [65,90,90,77,95,84,70,94,66,92,74,97,60,60,65,97,93,60,69,74]
+p2, p2cont = [77,94,71,63,96,82,85,75,72,91,99,63,84,87,79,94,90,60,69,62], [74,23,98,45,100,49,42]
+w, wcont = [80,87,68,72,66,77,99,85,70,93,98,72,100,89,67,86,91,79,71,99], [45,98,15,65,78,12,29]
+c = 900
+size = length(p1)
+
+@variable(m, x[1:size], Bin)
+@variable(m, 0 <= y[1:7] <= 1)
+@addobjective(m, Max, dot(x, p1))
+@addobjective(m, Max, dot(x, p2) + dot(y, p2cont))
+@constraint(m, dot(x, w) + dot(y, wcont)<= c)
+
+n = MOBC.solve_BC(m)
